e837697039c5f11c68d214af499b88bc5c318dd6
sinethemba7vitsha <vitsha709@gmail.com>
2025-03-06 10:26:47 +0200

    first commit

---
+++ Backend/src/main/java/com/example/WeBuyBusinesses/WeBuyBusinessesApplication.java
@@ -0,0 +1,13 @@
+package com.example.WeBuyBusinesses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeBuyBusinessesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeBuyBusinessesApplication.class, args);
+	}
+
+}
